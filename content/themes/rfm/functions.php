3cb3c089279f1e4eec227042e6944253c8d7feae
Derek Hubbard <derek.hubbard@pomiet.com>
2014-10-05 11:15:01 -0400

    Adding thumbnail size for featured images in sidebar. Modifying thumbnail size for grid view images on home page - making them a little wider.

--- content/themes/rfm/functions.php
+++ content/themes/rfm/functions.php
@@ -84,5 +84,6 @@ add_image_size('thumbnail-150', 150, 150, TRUE);
 add_image_size('thumbnail-175', 175, 175, TRUE);
 add_image_size('thumbnail-200', 200, 200, TRUE);
 add_image_size('thumbnail-300', 300, 300, TRUE);
+add_image_size('thumbnail-sidebar-featured', 300, 225, TRUE);
 add_image_size('thumbnail-meal-plan-image', 300, 300, TRUE);
-add_image_size('thumbnail-grid-post-image', 185, 250, TRUE);
+add_image_size('thumbnail-grid-post-image', 190, 250, TRUE);
